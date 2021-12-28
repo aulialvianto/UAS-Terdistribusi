596707b1b849c5487a9aa486ebe8238f98f2b9bd
aulialvianto <pitot.crush@gmail.com>
2021-12-28 09:10:25 +0700

    uas

---
+++ include/footer.php
@@ -0,0 +1,14 @@
+
+<div class="footer">
+	Developed by<br> College Projects for CS <br />
+</div>
+<?php
+//close connection
+if(isset($connect))
+mysql_close();
+?>
+
+<!--body closes-->
+</div>
+</body>
+</html>
